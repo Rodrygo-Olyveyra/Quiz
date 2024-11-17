815682c6b0c87934eec62a92a86355357e9bdb11
Rodrigo <rodrygo.olyveyra26@gmail.com>
2024-11-17 16:12:34 -0300

    Atualizações

--- lib/screens/Tela_de_Tempo.dart
+++ lib/screens/Tela_de_Tempo.dart
@@ -12,30 +12,54 @@ class TimeoutScreen extends StatelessWidget {
         title: const Text('Tempo Esgotado'),
         centerTitle: true,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              'lib/assests/Quiz Time.png',
-              height: 150,
-              fit: BoxFit.contain,
-            ),
-            const SizedBox(height: 20),
-            const Text(
-              'Você demorou muito para responder, comece novamente!',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-              textAlign: TextAlign.center,
-            ),
-            const SizedBox(height: 40),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).pop(); 
-                onRetry(); 
-              },
-              child: const Text('Reiniciar Quiz'),
-            ),
-          ],
+     body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color(0xFF1B0035), 
+              Color(0xFF00072D),
+              Color(0xFF2D004B), 
+            ],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'lib/assests/Quiz Time.png',
+                height: 150,
+                fit: BoxFit.contain,
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                'Você demorou muito para responder, comece novamente!',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 40),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                  onRetry();
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.orange,
+                  padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+                ),
+                child: const Text(
+                  'Reiniciar Quiz',
+                  style: TextStyle(fontSize: 18),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
